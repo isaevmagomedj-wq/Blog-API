f5e666984a3c2fd9f191000d8b2abe15ff4a7c08
isaevmagomedj-wq <isaevmagomedj@gmail.com>
2026-02-27 17:40:42 +0300

    Закончили проект

---
+++ posts/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    
+    # проверяет зарегались ли мы 
+    
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+# проверяет мы автор или нет
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
+
+    
---
+++ posts/serializers.py
@@ -0,0 +1,17 @@
+# serializers - переводит данные с базы в json 
+from django.contrib.auth import get_user_model
+from rest_framework import serializers 
+
+from .models import Post 
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username",)
+
